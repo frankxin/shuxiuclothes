5fa5e868ce19b4050522c141ecdadc4884119fd3
frankzhang <frankxin93@hotmail.com>
2014-07-24 16:27:14 +0800

    zzx 7/24 afternoon

--- Runtime/Cache/1439922bfc62f2fb4f51e056a0c01d92.php
+++ Runtime/Cache/1439922bfc62f2fb4f51e056a0c01d92.php
@@ -217,7 +217,7 @@
             <td><?php echo ($_SESSION['user']['real_name']); ?></td>
             <td><?php echo ($_SESSION['user']['phone']); ?></td>
             <td><?php echo ($_SESSION['user']['email']); ?></td>
-           	<td><a href="#">修改密码</a></td>
+           	<td><a href="#" data-toggle="modal" data-target="#myModal0">修改密码</a></td>
           </tr>
         </tbody>
     </table>
@@ -257,6 +257,43 @@
           $("#mylist").css("display","block");
           $("#myinfo").css("display","none");
       })
-</script> 
+</script>
+
+<!-- Modal -->
+<div class="modal fade" id="myModal0" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+        <h4 class="modal-title" id="myModalLabel">修改密码</h4>
+      </div>
+      <div class="modal-body">
+        <form class="form-horizontal" role="form">
+          <div class="form-group">
+            <label for="inputOldPassword" class="col-sm-2 control-label">新密码</label>
+            <div class="col-sm-10">
+              <input type="password" class="form-control" id="inputOldPassword" placeholder="New password">
+            </div>
+          </div>
+          <div class="form-group">
+            <label for="inputNewPassword" class="col-sm-2 control-label">确认新密码</label>
+            <div class="col-sm-10">
+              <input type="password" class="form-control" id="inputNewPassword" placeholder="Password">
+            </div>
+          </div>
+          <div class="form-group">
+            <div class="col-sm-offset-2 col-sm-10">
+              <button type="submit" class="btn btn-default">保存修改</button>
+            </div>
+          </div>
+        </form>
+      </div>
+ <!--      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">关闭</button>
+        <button type="button" class="btn btn-primary">保存修改</button>
+      </div> -->
+    </div>
+  </div>
+</div> 
 </body>
 </html>
